4abc1d6bd4c45b688ce996ea5cbef4af0942439d
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-11-13 21:20:20 -0300

    add filters component

---
+++ SeasonalFoods/Components/FiltersDto.cs
@@ -0,0 +1,12 @@
+﻿using SeasonalFoods.Enums;
+
+namespace SeasonalFoods.Components;
+
+public class FiltersDto
+{
+    public Seasons Season { get; set; } = null!;
+    public IEnumerable<string> FoodGroups { get; set; } = new List<string>();
+    public Regions Region { get; set; } = null!;
+    public IEnumerable<string> ShopPlace { get; set; } = new List<string>();
+    
+}
