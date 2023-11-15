5532c3ebe933cb748d188b7a1e25f129e3764817
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-11-15 01:52:20 -0300

    se añade componente filters

--- SeasonalFoods/Components/FiltersDto.cs
+++ SeasonalFoods/Components/FiltersDto.cs
@@ -4,9 +4,9 @@ namespace SeasonalFoods.Components;
 
 public class FiltersDto
 {
-    public Seasons Season { get; set; } = null!;
+    public string Season { get; set; } = null!;
     public IEnumerable<string> FoodGroups { get; set; } = new List<string>();
-    public Regions Region { get; set; } = null!;
+    public string Region { get; set; } = null!;
     public IEnumerable<string> ShopPlace { get; set; } = new List<string>();
     
 }
