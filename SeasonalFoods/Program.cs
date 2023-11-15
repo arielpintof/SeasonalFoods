40599cbfb4518879c0792704466b37aca8be97fa
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-11-15 01:32:52 -0300

    instalación de fluxor

--- SeasonalFoods/Program.cs
+++ SeasonalFoods/Program.cs
@@ -3,13 +3,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using SeasonalFoods;
 using MudExtensions.Services;
+using Fluxor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-
+var currentAssembly = typeof(Program).Assembly;
+builder.Services.AddFluxor(options => options.ScanAssemblies(currentAssembly));
 builder.Services.AddMudExtensions();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
