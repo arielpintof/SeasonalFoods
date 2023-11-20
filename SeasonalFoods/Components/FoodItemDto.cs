139beac7619ce7e3af1afef3160a15b5ec3a33e9
arielpintof <64876168+arielpintof@users.noreply.github.com>
2023-11-20 11:08:17 -0300

    commit para el eduardo

--- SeasonalFoods/Components/FoodItemDto.cs
+++ SeasonalFoods/Components/FoodItemDto.cs
@@ -11,12 +11,13 @@ public class FoodItemDto
 
 public class HistoricalData
 {
-    public Regions Region { get; set; } = null!;
-    public ShopPlaces ShopPlaces { get; set; } = null!;
-    public string Sector { get; set; } = string.Empty;
-    public double MinimalPrice { get; set; }
-    public double AveragePrice { get; set; }
-    public double MaximumPrice { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public Regions? Region { get; set; }
+    public ShopPlaces? ShopPlaces { get; set; } 
+    public string? Sector { get; set; } = string.Empty;
+    public double? MinimalPrice { get; set; }
+    public double? AveragePrice { get; set; }
+    public double? MaximumPrice { get; set; }
     public string? Variety { get; set; }
-    public string Date { get; set; } = string.Empty;
+    public string? Date { get; set; } = string.Empty;
 }
